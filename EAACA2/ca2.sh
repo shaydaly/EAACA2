7267f98d45b3e33e26925b9920af4bb54d224bb4
Seamus Daly <shaydaly20@gmail.com>
2016-12-16 13:30:23 +0000

    adding new results

--- EAACA2/ca2.sh
+++ EAACA2/ca2.sh
@@ -5,7 +5,7 @@ echo -e "IDLE \t\t N \t\t CO" >> results.dat
 for i in {1..40}
 do
     ./loadtest $i &
-    idle=$(mpstat 1 10 | awk 'END{print $NF}')
+    idle=$(mpstat 1 8 | awk 'END{print $NF}')
     pkill loadtest
     echo -e $idle "\t\t" $i "\t\t" $(wc -l synthetic.dat | awk '{print $1}') >> results.dat
 done
